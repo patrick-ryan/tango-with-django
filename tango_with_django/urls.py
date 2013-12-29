87515e01361c09633252aa51dd7dd37edc13b147
patrick-ryan <pryan@virginia.edu>
2013-12-29 01:06:43 -0500

    added templates

--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+# At the top of your urls.py file, add the following line:
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,4 +16,14 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
 )
+
+# UNDERNEATH your urlpatterns definition, add the following two lines:
+if settings.DEBUG:
+        urlpatterns += patterns(
+                'django.views.static',
+                (r'media/(?P<path>.*)',
+                'serve',
+                {'document_root': settings.MEDIA_ROOT}), )
