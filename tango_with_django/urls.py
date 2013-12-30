19775fed7e2fb23a0eeadd3bac22b3b85f725f7e
patrick-ryan <pryan@virginia.edu>
2013-12-30 17:52:54 -0500

    added models for db

--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,8 +16,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    
+
     url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
+    url(r'^admin/', include(admin.site.urls)), # ADD THIS LINE
 )
 
 # UNDERNEATH your urlpatterns definition, add the following two lines:
