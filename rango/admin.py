7f395a854d6ef273b551bd339b699cd2e74cbe1c
patrick-ryan <pryan@virginia.edu>
2013-12-31 00:18:31 -0500

    redesigned admin interface

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['title', 'category', 'url']}),
+        ('View information', {'fields': ['views']}),
+    ]
+    list_display = ('title', 'category', 'url')
+    list_filter = ['views']
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
