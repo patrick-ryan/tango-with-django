d98fbf9201b1ac9f21a0a58659abb7b21044640f
patrick-ryan <pryan@virginia.edu>
2014-01-08 11:26:13 -0500

    added template hierarchy

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -12,6 +12,8 @@ DATABASE_PATH = os.path.join(PROJECT_PATH, 'rango.db')
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+LOGIN_URL = '/rango/login/'
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
